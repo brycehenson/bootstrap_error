c4bfe6f988552bda6647854752071266cda0584f
Bryce Henson <bryce.henson@live.com>
2019-06-07 18:26:24 +1000

    added nan handling to unc_wmean

--- lib/unc_wmean.m
+++ lib/unc_wmean.m
@@ -1,17 +1,27 @@
 function [mean_val,ste_mean]=unc_wmean(x,unc)
+%TODO:
+% -standard format
+% -docs, tests
+% - cleaner layout
 
 x=col_vec(x);
 unc=col_vec(unc);
 if numel(unc)~=1 && numel(x)~=numel(unc)
     error('unc must be the same size as data vec or single element')
 end
-nan_mask=isnan(x) | isnan(unc);
+
+if numel(unc)==1
+    nan_mask=isnan(x);
+    x=x(~nan_mask);
+else
+    nan_mask=isnan(x) | isnan(unc);
+    x=x(~nan_mask);
+    unc=unc(~nan_mask);
+end
+
 if sum(nan_mask)~=0
     warning('%s: nan found in data will ignore these points\n',mfilename)
 end
-
-x=x(~nan_mask);
-unc=unc(~nan_mask);
 
 if numel(x)==0
     warning('%s: no data pts remaining after removing nans will retun nans',mfilename)
