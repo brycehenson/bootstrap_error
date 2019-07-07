26da1903be81b5889f249c8974928b65e6192f51
Bryce <bryce.henson@live.com>
2019-07-07 21:18:44 +1000

    normal correction c4 can now do a vector of values

--- lib/normal_correction_c4.m
+++ lib/normal_correction_c4.m
@@ -12,10 +12,13 @@ elseif n>1e7
     c4n=1; %gives an error of 2e-7
 else
     c4n=sqrt(2./(n-1)).*(gamma(n/2)./gamma((n-1)./2));
-    if isnan(c4n)%handle numerical overflow and use the vpa toolbox (slower)
+    nan_mask=isnan(c4n);
+    if sum(nan_mask)>0 %handle numerical overflow and use the vpa toolbox (slower)
         %warning('using vpa')
-        n=vpa(n);
-        c4n=sqrt(2./(n-1)).*(gamma(n./2)./gamma((n-1)./2));
+        for ii=1:numel(c4n)
+            n_vpa=vpa(n(nan_mask));
+            c4n(nan_mask)=sqrt(2./(n_vpa-1)).*(gamma(n_vpa./2)./gamma((n_vpa-1)./2));
+        end
     end
 end
 out=c4n;
