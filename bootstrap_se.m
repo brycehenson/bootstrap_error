d4496a66888e45fd514f19ba4d59273e9907f6c8
Bryce Henson <bryce.henson@live.com>
2021-08-17 22:23:12 +1000

    fixed issues with passing the sampling fraction and with the minimum data size that was used when the est. function was tested for the output vector size

--- bootstrap_se.m
+++ bootstrap_se.m
@@ -20,7 +20,7 @@ function [out,detailed_data_out]=bootstrap_se(est_fun_in,data,varargin)
 % '[matlab][bootstrap_error]' in the subject line for me to respond
 % Last revision:2019-06-02
 
-min_sample_num=5; %minimum data size for resampling
+
 
 p = inputParser;
 is_lims=@(x) (isequal(size(x),[1,2]) && (isnumeric(x) && x(2)<10 && x(1)>0))...
@@ -44,7 +44,12 @@ addOptional(p,'plot_fig_name','',@ischar);
 addOptional(p,'do_mean_fit',1,is_c_logical)
 addOptional(p,'use_weighting',true,is_c_logical)
 addOptional(p,'norm_weighting',false,is_c_logical)
-addOptional(p,'use_frac_size',false,is_c_logical)
+     % use the second argument of the estimation function as the fraction of the data that will be used
+     % can be usefull for simulating lower detection efficiency
+addOptional(p,'pass_samp_frac_to_est_size',false,is_c_logical) % this is the old name will be retired soon
+addOptional(p,'pass_sample_frac',false,is_c_logical) % this is the old name will be retired soon
+
+   
 parse(p,varargin{:});
 
 do_mean_fit=p.Results.do_mean_fit;
@@ -53,7 +58,11 @@ do_replace=coerce_logical(p.Results.replace);
 use_mean_se_for_se_se=coerce_logical(p.Results.mean_se_for_se_se);
 use_weighting=coerce_logical(p.Results.use_weighting);
 norm_weighting=coerce_logical(p.Results.norm_weighting);
-use_frac=coerce_logical(p.Results.use_frac_size); %do we want to use the fractional method
+pass_samp_frac_to_est=coerce_logical(p.Results.pass_samp_frac_to_est_size) | coerce_logical(p.Results.pass_sample_frac); 
+
+if coerce_logical(p.Results.pass_sample_frac)
+    warning('argument pass_samp_frac_to_est_size is depricated and is now renamed pass_sample_frac option')
+end
 
 if use_mean_se_for_se_se
     warning('mean_se_for_se_se is depricated')
@@ -71,14 +80,21 @@ if p.Results.num_samp_frac==1
      sample_frac_vec=mean(p.Results.samp_frac_lims);
     %sample over multiple fractions
 elseif size(p.Results.samp_frac_lims,2)==2 
+    samp_lims=p.Results.samp_frac_lims;
+    num_samp=p.Results.num_samp_frac;
     if ~p.Results.samp_frac_log
-        sample_frac_vec=linspace(p.Results.samp_frac_lims(1),...
-            p.Results.samp_frac_lims(2),p.Results.num_samp_frac)';
+        sample_frac_vec=linspace(samp_lims(1),...
+            samp_lims(2),num_samp)';
     else
-        sample_frac_vec=logspace(log10(p.Results.samp_frac_lims(1)),...
-            log10(p.Results.samp_frac_lims(2)),p.Results.num_samp_frac)';
+        sample_frac_vec=logspace(log10(samp_lims(1)),...
+            log10(samp_lims(2)),num_samp)';
     end
+else
+    error('limits should be a vector of 2 numbers')
 end
+
+
+
 %overwrite plots if only samplign at one fraction of the data
 %if size(sample_frac_vec,1)==1
 %   do_plots=false;
@@ -86,6 +102,7 @@ end
 
 n_total=numel(data);
 sample_num_vec=floor(sample_frac_vec*n_total);
+min_sample_num=min(sample_num_vec); %minimum data size used for testing the estimation function
 sample_frac_vec=sample_num_vec/n_total;
 %cull anything below min_num
 mask=sample_num_vec>=min_sample_num;
@@ -120,13 +137,15 @@ else
     est_fun_multi_out=false;
 end
 
-if use_frac 
+%% find the size of the scalar output by calling the estimation function once
+
+% for finding the size of the output of the estimation function we will temporarily pass a sample fraction of 1
+if pass_samp_frac_to_est 
     est_fun =  @(data,opts) est_fun_in(data,1,opts);
 else
     est_fun =  est_fun_in;
 end
 
-%% find the size of the scalar output by calling the estimation function once
 data_smpl=randsample(data,min_sample_num,do_replace);
 if est_fun_multi_out
     [out_cell_tmp{:}]=est_fun(data_smpl,opp_arguments{:});
@@ -135,10 +154,16 @@ else
     out_val_tmp=est_fun(data_smpl,opp_arguments{:});
 end 
 if ~isvector(out_val_tmp)
-    erro('first output of function is not a scalar or vector')
+    error('first output of function is not a scalar or vector')
 end
 out_val_tmp=col_vec(out_val_tmp);
 output_val_size=numel(out_val_tmp);
+
+% remove the temp definition used to fool the size finding above
+if pass_samp_frac_to_est 
+    est_fun =  est_fun_in; 
+end
+
 %%
 %prealocate the moments of the distribution
 mean_sub=NaN(numel(sample_frac_vec),output_val_size);
@@ -152,8 +177,8 @@ for ii=1:iimax
     n_sample=sample_num_vec(ii);
     frac_sample=sample_frac_vec(ii);
     %std means nothing for n<3
-    %the finte sample correaction for the no replacements method breaks when n_sample=ntot
-    if (n_sample>3 && (n_sample<n_total || do_replace)) || (use_frac && frac_sample<=1.0)
+    %the finte sample correction for the no replacements method breaks when n_sample=ntot
+    if (n_sample>3 && (n_sample<n_total || do_replace)) || (pass_samp_frac_to_est && frac_sample<=1.0)
         est_fun_res_sub=NaN(repeat_samp(ii),output_val_size); %the results of the estimation function
         if do_replace
             finite_pop_corr=1;
@@ -162,14 +187,14 @@ for ii=1:iimax
         end 
         for jj=1:repeat_samp(ii)
             %calculate the analysis operation on the subset of dat
-            if ~use_frac
+            if ~pass_samp_frac_to_est
                 data_smpl=randsample(data,n_sample,do_replace);
             end
             %then we assign the output of the est_fun on the data_smp to est_fun_res_sub
             %matalb cant do [out_cell_tmp{:}]=scalar so a case statement
             %is needed
             if est_fun_multi_out
-                if use_frac
+                if pass_samp_frac_to_est
                     [out_cell_tmp{:}]=est_fun(data_smpl,frac_sample,opp_arguments{:});
                 else
                     [out_cell_tmp{:}]=est_fun(data_smpl,opp_arguments{:});
@@ -181,7 +206,7 @@ for ii=1:iimax
                 est_fun_res_sub(jj,:)=out_val_tmp;
                 out_cell{ii,jj}=out_cell_tmp{2:end};
             else
-                if use_frac
+                if pass_samp_frac_to_est
                     out_val_tmp=est_fun(data_smpl,frac_sample,opp_arguments{:});
                 else
                     out_val_tmp=est_fun(data_smpl,opp_arguments{:});
